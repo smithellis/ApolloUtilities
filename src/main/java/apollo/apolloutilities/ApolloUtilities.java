7577f5fd5a278729fcdfdb8cf116d5674a8ff1b0
BuildTools <unconfigured@null.spigotmc.org>
2022-06-01 20:18:23 -0400

    Third commit

--- src/main/java/apollo/apolloutilities/ApolloUtilities.java
+++ src/main/java/apollo/apolloutilities/ApolloUtilities.java
@@ -1,23 +1,27 @@
 package apollo.apolloutilities;
 
+import jdk.vm.ci.code.Register;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+
+
 public final class ApolloUtilities extends JavaPlugin implements Listener {
+
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         System.out.println("Apollo Utilities Is Running Correctly!");
-
+        getServer().getPluginManager().registerEvents(this,this);
     }
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        System.out.println("Welcome to the Apollo Network " + event.getPlayer().getName());
+    public void onPlayerJoin(PlayerJoinEvent event){
+        event.setJoinMessage("Welcome to the Apollo Network " + event.getPlayer().getDisplayName() + "!");
     }
 
 
