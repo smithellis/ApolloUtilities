a39298b2688207c60cb49eff37ad6dd8c82bc9cb
BuildTools <unconfigured@null.spigotmc.org>
2022-06-03 20:51:38 -0400

    Added ranks and stream command

--- src/main/java/apollo/apolloutilities/ApolloUtilities.java
+++ src/main/java/apollo/apolloutilities/ApolloUtilities.java
@@ -2,45 +2,43 @@ package apollo.apolloutilities;
 
 import apollo.apolloutilities.commands.ApolloCommands;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.enchantments.EnchantmentTarget;
-import org.bukkit.enchantments.EnchantmentWrapper;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PiglinBarterEvent;
-import org.bukkit.event.inventory.CraftItemEvent;
-import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.checkerframework.checker.nullness.qual.NonNull;
-
-import javax.swing.text.html.parser.Entity;
-import java.util.Locale;
 
 public final class ApolloUtilities extends JavaPlugin implements Listener{
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        //define where to execute command
+        ApolloCommands commands = new ApolloCommands();
+
+        //message on start up
         System.out.println(ChatColor.GOLD + "Apollo Utilities Is Running!");
+
+        //event listener
         getServer().getPluginManager().registerEvents(this,this);
-        getCommand("hub").setExecutor(new ApolloCommands());
+
+        //hub command
+        getCommand("hub").setExecutor(commands);
+        getCommand("stream").setExecutor(commands);
+        getCommand("ranks").setExecutor(commands);
     }
 
+    //event when player joins (sets a new join message with color)
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event){
         event.setJoinMessage(ChatColor.LIGHT_PURPLE + "Welcome to the Apollo Network " + ChatColor.GREEN + event.getPlayer().getDisplayName() + ChatColor.LIGHT_PURPLE + "!");
         Player player = event.getPlayer();
-        player.setGravity(false);
+        player.setGravity(true);
     }
 
 
 
 
+    //all information saves and plugins sends the stop message
     @Override
     public void onDisable() {
         // Plugin shutdown logic
--- src/main/java/apollo/apolloutilities/commands/ApolloCommands.java
+++ src/main/java/apollo/apolloutilities/commands/ApolloCommands.java
@@ -1,6 +1,7 @@
 package apollo.apolloutilities.commands;
 
 import com.sun.org.apache.xpath.internal.operations.Or;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,15 +13,18 @@ public class ApolloCommands implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (sender instanceof Player) {
-            Player player = (Player) sender;
-            if (cmd.getName().equalsIgnoreCase("hub".toLowerCase(Locale.ROOT))) {
-                player.performCommand("warp lobby");
-            }
+        if (!(sender instanceof Player)) {sender.sendMessage("Please only use this as a player!");}
+        Player player = (Player) sender;
+        if (cmd.getName().equalsIgnoreCase("hub")) {
+            player.performCommand("warp lobby");
+            sender.sendMessage(ChatColor.GOLD + "(!) " + ChatColor.LIGHT_PURPLE + "Attempting to send you to the lobby...");
         }
-
-        System.out.println("Please only use this as a player!");
-
+        if (cmd.getName().equalsIgnoreCase("stream")) {
+            sender.sendMessage(ChatColor.GOLD + "(!) " + ChatColor.LIGHT_PURPLE + "Go watch Herculuke on twitch (even if he isn't live): https://www.twitch.tv/herculuke");
+        }
+        if (cmd.getName().equalsIgnoreCase("ranks")) {
+            sender.sendMessage(ChatColor.GOLD + "(!) " + ChatColor.LIGHT_PURPLE + "All ranks and perks available to buy here: https://apollo-shop.tebex.io/category/ranks");
+        }
         return true;
     }
 }
